d186d66a6b8d9a33c9b54ae2d42e1f7069301685
Denis Sedov <CerberBoss@mail.ru>
2016-04-28 16:28:54 +0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
 Player.delete_all
+Ball.delete_all
 
 @@colorArray = [0xFF0000,0xFF6600,0xFFCC00,0x66CC00,0x6699FF,0x3333FF,0x660066]
 
-Player.create!([
-  {idplayer: 1111111, name: "test1", color: @@colorArray[0], x: 30, y: 50, radius: 5},
-  {idplayer: 2222222, name: "test2", color: @@colorArray[3], x: 70, y: 80, radius: 5},
-  {idplayer: 3333333, name: "test3", color: @@colorArray[5], x: 100, y: 120, radius: 5}
-  ])
+
+#Player.create!([
+#  {idplayer: 1111111, name: "test1", color: @@colorArray[0], x: 30, y: 50, radius: 5},
+#  {idplayer: 2222222, name: "test2", color: @@colorArray[3], x: 70, y: 80, radius: 5},
+#  {idplayer: 3333333, name: "test3", color: @@colorArray[5], x: 100, y: 120, radius: 5}
+#  ])
